84d287db1b43401dd348f7017dcdaf50b3d1d53e
MLecora <mia@mail.it>
2019-11-22 12:45:33 +0100

    End of section 4

--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -6,20 +7,30 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  @Input() valuesFromHome: any;
+  @Output() cancelRegister = new EventEmitter();
   model: any = {};
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
   }
 
-  register(){
-    console.log(this.model);
+  register() {
+    this.authService.register(this.model).subscribe(
+      () => {
+        console.log('registration successful');
+      },
+      error => {
+        console.log(error);
+      },
+      () => {
+        console.log('registration service invocation completed')
+      }
+    );
   }
 
-  cancel()
-  {
+  cancel() {
+    this.cancelRegister.emit(false);
     console.log('cancelled');
   }
 
